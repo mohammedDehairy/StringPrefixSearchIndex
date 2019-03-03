3245f0a4f13527bd86cfd3bc119f4c2b328e44cc
Mohamed Mohamed El Dehairy <m.eldehairy@gmail.com>
2019-03-03 15:08:22 +0100

    Initial Commit

---
+++ StringPrefixSearchIndex/StringPrefixSearchIndex.h
@@ -0,0 +1,19 @@
+//
+//  StringPrefixSearchIndex.h
+//  StringPrefixSearchIndex
+//
+//  Created by mohamed mohamed El Dehairy on 3/3/19.
+//  Copyright © 2019 mohamed El Dehairy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StringPrefixSearchIndex.
+FOUNDATION_EXPORT double StringPrefixSearchIndexVersionNumber;
+
+//! Project version string for StringPrefixSearchIndex.
+FOUNDATION_EXPORT const unsigned char StringPrefixSearchIndexVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StringPrefixSearchIndex/PublicHeader.h>
+
+
